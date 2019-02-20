f2d5acb61064b8acb0e14420cda7d259ec7db26c
Aliya Summers <aliyasummers1@gmail.com>
2019-02-20 14:35:59 -0800

    more bugs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 //click number countdown
-var total_clicks = 2;
+var total_clicks = 10;
 //objects from constructor instances get moved into here
 var all_mall_items = [];
 var temp_mall_items = [];
@@ -33,6 +33,10 @@ var Mall_item = function (item_name, filepath) {
   //pushes instances into the global array
   all_mall_items.push(this);
   temp_mall_items.push(this);
+
+  //console.log('all in constructor', all_mall_items);
+  //console.log('temp in constructor', temp_mall_items);
+
 };
 
 //Makes images log to page
@@ -49,6 +53,8 @@ var render_products = function(product, target_img, target_h2){
 
 //Start of main function that runs the entire program
 var handle_click_on_item = function(event) {
+  console.log('temp mall items after handler', temp_mall_items);
+  console.log('all mall items after handler', all_mall_items);
   console.log('click');
 
   //registers that the click happened on an image
@@ -57,10 +63,13 @@ var handle_click_on_item = function(event) {
   //been clicked on.
     if(event.target.id === 'left_img'){
       currently_displayed_left_product.num_times_clicked ++;
+      console.log('you have clicked left', currently_displayed_left_product.item_name);
     } else if(event.target.id === 'center_img'){
       currently_displayed_center_product.num_times_clicked ++;
+      console.log('you have clicked center', currently_displayed_center_product.item_name);
     } else if (event.target.id === 'right_img'){
       currently_displayed_right_product.num_times_clicked ++;
+      console.log('you have clicked right', currently_displayed_right_product.item_name);
     }
 
     //decrements my click counter
@@ -74,27 +83,28 @@ var handle_click_on_item = function(event) {
     //bit of code runs
 
     //starts splice to control randomization
-    debugger;
+    //debugger;
     var left_img_idx = Math.floor(Math.random() * temp_mall_items.length);
-    console.log('temp left', temp_mall_items);
+    console.log('temp mall items after random', temp_mall_items);
+    console.log('all mall items after random', all_mall_items);
     
     temp_mall_items.splice(left_img_idx, 1);
     render_products(temp_mall_items[left_img_idx], left_img, left_h2);
     currently_displayed_left_product = temp_mall_items[left_img_idx];
-    console.log('displayed left: ', currently_displayed_left_product);
+    //console.log('displayed left: ', currently_displayed_left_product);
 
     var center_img_idx = Math.floor(Math.random() * temp_mall_items.length);
     temp_mall_items.splice(center_img_idx, 1);
     render_products(temp_mall_items[center_img_idx], center_img, center_h2);
     currently_displayed_center_product = temp_mall_items[center_img_idx];
-    console.log('center left: ', currently_displayed_center_product);
+    //console.log('center left: ', currently_displayed_center_product);
 
 
     var right_img_idx = Math.floor(Math.random() * temp_mall_items.length);
     render_products(temp_mall_items[right_img_idx], right_img, right_h2);
     temp_mall_items.splice(right_img_idx, 1);
     currently_displayed_right_product = temp_mall_items[right_img_idx];
-    console.log('displayed right: ', currently_displayed_right_product);
+    //console.log('displayed right: ', currently_displayed_right_product);
 
     //set up splice here
     //spread operator - feeds values into target array
@@ -127,6 +137,10 @@ var handle_click_on_item = function(event) {
 new Mall_item('R2D2 Bag', './img/bag.jpg');
 new Mall_item('Banana Cutter', './img/banana.jpg');
 new Mall_item('Bathroom iPad', './img/bathroom.jpg');
+new Mall_item('Boots', './img/boots.jpg');
+new Mall_item('Breakfast', './img/breakfast.jpg');
+new Mall_item('Bubblegum', './img/bubblegum.jpg');
+new Mall_item('Chair', './img/chair.jpg');
 new Mall_item('Cthulhu', './img/cthulhu.jpg');
 new Mall_item('Duck Bill', './img/dog-duck.jpg');
 new Mall_item('Dragon Meat', './img/dragon.jpg');
