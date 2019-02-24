c6a65ba8231dfe60d21ec752d76fd6c044237a98
Aliya Summers <aliyasummers1@gmail.com>
2019-02-24 11:11:16 -0800

    running into erros when I try to fix chart, calling it good enough

--- js/app.js
+++ js/app.js
@@ -82,14 +82,14 @@ var render_products_shown_chart = function (){
 //begin chart
 var render_chart = function(data, labels, title, ctx){
   new Chart(ctx, {
-    type: 'pie',
+    type: 'horizontalBar',
     data: {
       labels: labels,
       datasets: [{
         label: title,
         data: data,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
+          'rgba(255, 99, 132, 0.0)',
           'rgba(54, 162, 235, 0.7)',
           'rgba(255, 206, 86, 0.7)',
           'rgba(75, 192, 192, 0.7)',
@@ -108,10 +108,11 @@ var render_chart = function(data, labels, title, ctx){
           'rgba(255, 206, 86, 0.7)',
           'rgba(75, 192, 192, 0.7)',
           'rgba(153, 102, 255, 0.7)',
-          'rgba(255, 159, 64, 0.7)'
+          'rgba(252, 252, 10, 0.7)',
+          'rgba(48, 13, 153, 1)'
         ],
         borderColor: [
-          'rgba(255,99,132,1)',
+          'rgba(255,99,132, 0.1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
@@ -130,7 +131,8 @@ var render_chart = function(data, labels, title, ctx){
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(252, 252, 10, 1)',
+          'rgba(48, 13, 153, 1)'
         ],
         borderWidth: 1
       }]
@@ -269,7 +271,7 @@ var initial_images = function () {
 
 if(total_clicks === 26){
   initial_images();
-  // all_mall_items.shift();
+  //all_mall_items.shift();
   // temp_mall_items.shift();
 }
 
